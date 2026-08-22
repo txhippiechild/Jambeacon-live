269bd85a1de1215ed9e8b37fc2b7f88ce89c8a3d
txhippiechild <texashippiechild@gmail.com>
2026-08-22 12:57:05 -0500

    Replace legacy site with JamBeacon avatar experience

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "jambeacon-avatar-v1";
+const APP_SHELL = [
+  "/",
+  "/manifest.webmanifest",
+  "/jambeacon-icon.svg",
+  "/avatar-male.png",
+  "/avatar-female.png",
+  "/jam-chat-scene.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok) caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
+          return response;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
